c49157dfb32d20f156af7b8a17faf79e0c90a2ae
卢森 <lusen_cn@163.com>
2017-06-07 16:40:43 +0800

    组件更新

---
+++ src/lib/widget/grid/index.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="w-grid-ct" :style="ctStyle">
+        {this.headerView()}
+        {this.bodyView()}
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        // 列表border宽度
+        borderSpacing: Number,
+        // 列属性
+        // 例：[{
+        //      bodyCellStyle: <表体单元格样式（object）>,
+        //      bodyCellView: <表体单元格视图（func）>,
+        //      bodyTdStyle: <表体Td元素样式（object）>,
+        //      headerCellStyle: <表头单元格样式（object）>,
+        //      headerCellView: <表头单元格视图（func）>,
+        //      headerTdStyle: <表头Td元素样式（object）>,
+        //      label: <显示名称（必选）>,
+        //      name: <字段名称（必选）>,
+        //      width: <宽度>
+        //  }, ……]
+        columns: {
+            type: Array,
+            required: true
+        },
+        // 列表空数据视图
+        emptyView: Function,
+        // 表头高度
+        headerHeight: Number,
+        // 表头是否固定，不随滚动条滚动
+        isFixHeader: Boolean,
+        // 列表数据
+        records: {
+            type: Array,
+            required: true
+        },
+        // 列表容器样式
+        style: Object,
+        // 列表容器宽度
+        width: Number
+
+    },
+    data() {
+        // 表头悬浮显示时，表体容器样式
+        this.isFixHeader && (this.fixedCtStyle = {
+            top: (headerHeight || 30) + 'px'
+        });
+
+        // 表头/表体table样式
+        this.tableStyle = {
+            width: width,
+            borderSpacing: borderSpacing || 1
+        };
+        return {
+            // 列表容器样式
+            ctStyle: Object.assign({}, this.isFixHeader ? {
+                    position: 'relative'
+                } : null,
+                style, {
+                    width: this.width
+                }
+            )
+        }
+    },
+    //--------------------------------------------------------------------------
+    _initStyle() {
+        // 列表容器样式
+        this.ctStyle = Object.assign({}, this.isFixHeader ? {
+                position: 'relative'
+            } : null,
+            style, {
+                width: width
+            }
+        );
+
+        // 表头悬浮显示时，表体容器样式
+        this.isFixHeader && (this.fixedCtStyle = {
+            top: (this.headerHeight || 30) + 'px'
+        });
+
+        // 表头/表体table样式
+        this.tableStyle = {
+            width: this.width,
+            borderSpacing: this.borderSpacing || 1
+        };
+
+        this._calColWidth();
+    },
+    /**
+	 * 计算未设置width值的列的宽度（如果有列设置width为百分数，或width设置不规范，则不执行该计算）
+	 */
+    _calColWidth() {
+        this.calColWidthVal = 'auto';
+
+        let {borderSpacing, columns, width} = this.props;
+        if (!width || !isNumber(width)) {
+            return;
+        }
+        if (columns == null) {
+            return;
+        }
+
+        // 是否计算每列宽度
+        let isNoCal = false;
+        // 未设置宽度的列的数量
+        let noWidthCnt = 0;
+        // 用于平均计算的宽度值
+        let minusResult = width - (columns.length + 1) * (borderSpacing || 1);
+        columns.some(column => {
+            let itemWidth = column.width;
+            if (!itemWidth) {
+                noWidthCnt++;
+                return false;
+            }
+            if (itemWidth && (itemWidth *= 1) && !isNumber(itemWidth)) {
+                // 有列设置了非数字的宽度，导致无法为每列计算宽度
+                isNoCal = true;
+                return true;
+            }
+            minusResult -= itemWidth;
+        });
+
+        if (!isNoCal && noWidthCnt) {
+            this.calColWidthVal = minusResult / noWidthCnt
+        }
+        (this.calColWidthVal <= 0) && (this.calColWidthVal = 'auto');
+    }
+}
+</script>
+
+<style scoped>
+</style>
