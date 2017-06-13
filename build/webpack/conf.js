90febb8b0a5adc68d0799b811e2fad6ea6231d54
lusen <lusen@chanjet.com>
2017-06-13 10:32:23 +0800

    配置

--- build/webpack/conf.js
+++ build/webpack/conf.js
@@ -26,79 +26,93 @@ module.exports = {
         // html页面引用JS/CSS文件的前缀路径
         publicPath: config.resUrlPre,
         // 构建后入口文件的相对路径
-        filename: debug ? '[name].js': '[name]/[chunkhash].js',
+        filename: debug
+            ? '[name].js'
+            : '[name]/[chunkhash].js',
         // 构建后动态加载文件的相对路径
-        chunkFilename: debug ? '[name].js' : '[name]/[chunkhash].js'
+        chunkFilename: debug
+            ? '[name].js'
+            : '[name]/[chunkhash].js'
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader'
-        }, {
-            test: /\.vue$/,
-            loader: 'vue-loader'
-        }, {
-            // 因CSS构建后的路径变化，将CSS中图片的路径修改为指定路径，并将图片复制到对应目录
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: `${config.imgUrlPre}/[name]${debug ? '' : '/[hash:8]'}.[ext]`
-            }
-        }, {
-            // 因CSS构建后的路径变化，将CSS中字体的路径修改为指定路径，并将字体复制到对应目录
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: `${config.fontUrlPre}/[name]${debug ? '' : '/[hash:8]'}.[ext]`
-            }
-        }]
-    },
-    resolve: {
-        alias: {
-            'fe-util': 'lib/util',
-            'vue-widget': 'lib/widget'
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        css: ExtractTextPlugin.extract({
+                            use: {
+                                loader: 'css-loader',
+                                fallback: 'vue-style-loader'
+                            }
+                        })
+                    }
+                }, {
+                    // 因CSS构建后的路径变化，将CSS中图片的路径修改为指定路径，并将图片复制到对应目录
+                    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: `${config.imgUrlPre}/[name]${debug ? '' : '/[hash:8]'}.[ext]`
+                    }
+                }, {
+                    // 因CSS构建后的路径变化，将CSS中字体的路径修改为指定路径，并将字体复制到对应目录
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: `${config.fontUrlPre}/[name]${debug ? '' : '/[hash:8]'}.[ext]`
+                    }
+                }
+            ]
         },
-        extensions: ['.js', '.jsx', '.vue'],
-        modules: [config.dir.src, 'node_modules']
-    },
-    plugins: (() => {
-        let arr = [
-            // 将chunks中对应的entry所引用的公共文件提取出来，chunk名为common
-            new CommonsChunkPlugin({
-                name: 'common',
-                chunks: Object.keys(entry)
-            }),
-            new CommonsChunkPlugin({
-                name: 'lib',
-                filename: 'lib.js',
-                chunks: ['lib', 'common']
-            })
-            // 将JS代码中引用的CSS文件，合并提取成一个CSS文件
-            /*new ExtractTextPlugin(
-                debug ? '[name].css' : '[name]/[contenthash:8].css',
-                {allChunks: true}
-            )*/
-        ];
+        resolve: {
+            alias: {
+                'fe-util': 'lib/util',
+                'vue-widget': 'lib/widget'
+            },
+            extensions: [
+                '.js', '.jsx', '.vue'
+            ],
+            modules: [config.dir.src, 'node_modules']
+        },
+        plugins: (() => {
+            let arr = [
+                // 将chunks中对应的entry所引用的公共文件提取出来，chunk名为common
+                new CommonsChunkPlugin({name: 'common', chunks: Object.keys(entry)}),
+                new CommonsChunkPlugin({
+                    name: 'lib',
+                    filename: 'lib.js',
+                    chunks: ['lib', 'common']
+                }),
+                // 将JS代码中引用的CSS文件，合并提取成一个CSS文件
+                new ExtractTextPlugin(
+                    debug ? '[name].css' : '[name]/[contenthash:8].css',
+                    {allChunks: true}
+                )
+            ];
 
-        !debug && arr.push(
-            // 设置webpack环境变量
-            new DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': '"' + envConfig.env + '"'
-                }
-            }),
-            // JS文件压缩（去空格注释，变量替换）
-            new UglifyJsPlugin({
-                compress: {
-                    warnings: false
-                }
-            })
-        );
+            !debug && arr.push(
+                // 设置webpack环境变量
+                new DefinePlugin({
+                    'process.env': {
+                        'NODE_ENV': '"' + envConfig.env + '"'
+                    }
+                }),
+                // JS文件压缩（去空格注释，变量替换）
+                new UglifyJsPlugin({
+                    compress: {
+                        warnings: false
+                    }
+                })
+            );
 
-        // 将构建生成的JS文件添加到指定html文件中
-        /*Object.keys(entry).forEach(chunk => {
+            // 将构建生成的JS文件添加到指定html文件中
+            /*Object.keys(entry).forEach(chunk => {
             arr.push(new HtmlWebpackPlugin({
                 template: path.join(config.dir.tpl, 'index.html'),
                 filename: path.join(config.dir.release, `${chunk}.html`),
@@ -107,23 +121,25 @@ module.exports = {
             }));
         });*/
 
-        Object.keys(entry).forEach(chunk => {
-            arr.push(new HtmlWebpackPlugin({
-                chunks: [chunk],
-                filename: path.resolve(config.dir.html, `${chunk}_css.html`),
-                inject: false,
-                template: path.resolve(config.dir.tpl, 'css.html')
-            }));
+            Object.keys(entry).forEach(chunk => {
+                arr.push(new HtmlWebpackPlugin({
+                    chunks: [chunk],
+                    filename: path.resolve(config.dir.html, `${chunk}_css.html`),
+                    inject: false,
+                    template: path.resolve(config.dir.tpl, 'css.html')
+                }));
 
-            arr.push(new HtmlWebpackPlugin({
-                chunks: ['lib', 'common', chunk],
-                filename: path.resolve(config.dir.html, `${chunk}_js.html`),
-                inlineNames: ['jsInline'],
-                inject: false,
-                template: path.resolve(config.dir.tpl, 'js.html')
-            }));
-        });
+                arr.push(new HtmlWebpackPlugin({
+                    chunks: [
+                        'lib', 'common', chunk
+                    ],
+                    filename: path.resolve(config.dir.html, `${chunk}_js.html`),
+                    inlineNames: ['jsInline'],
+                    inject: false,
+                    template: path.resolve(config.dir.tpl, 'js.html')
+                }));
+            });
 
-        return arr;
-    })()
-}
+            return arr;
+        })()
+    }
