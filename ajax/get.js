174baca6ceb603ea8b281c16ff3284ad8b4f51c2
lusen <lusen@chanjet.com>
2017-06-15 14:22:03 +0800

    Squashed 'src/lib/util/' changes from eebff5c..571444a
    571444a ajax api变更
    
    git-subtree-dir: src/lib/util
    git-subtree-split: 571444aefcdc390840c0b352eade170390daa27f

--- ajax/get.js
+++ ajax/get.js
@@ -1,22 +1,22 @@
 import isString from '../is/isString';
 import ajaxEventSource from './event';
-import reqwest from 'reqwest';
+import axios from 'axios';
 
 /**
  * Ajax Get请求
  */
 let get = function(params = {}) {
-	isString(params) && (params = { url: params });
+	if (!isString(params)) {
+		params = {
+			method: 'get',
+			url: params.url,
+			params: params.data
+		}
+	}
 
-	var _params = { data: params.data, method: 'GET' };
-	ajaxEventSource.trigger('beforeAjax', _params);
-	params.data = _params.data;
-
-	return reqwest(params).then(response => { 
-		ajaxEventSource.trigger('afterAjaxSuccess', response);
+	return reqwest(params).then(response => {
 		return response;
 	}, ...args => {
-		ajaxEventSource.trigger('afterAjaxFailure', args);
 		return args[0];
 	});
 }
